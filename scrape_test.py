53a53902f45f2fc1e990ec1f5c84b82bfee4cb20
NathanKMuir <64637150+NathanKMuir@users.noreply.github.com>
2021-12-06 00:56:29 +1000

    Add files via upload
    Update 2 - workable version with significant upgrades in pre-processing and cleaning. Unlikely to be many changes made to this section of the code.

--- scrape_test.py
+++ scrape_test.py
@@ -19,29 +19,34 @@ import seaborn as sns
 import nltk
 import re
 from statistics import mean
+from itertools import chain
 
 # Set working directory
 wd = "C:\\Users\\natha\\Documents\\Master of Data Science\\MA5851 Natural Language Processing\\A3"
 os.chdir(wd)
 
+# Use TwitterSearchScraper to append tweets to list
+def search_twitter(search_terms, since, until):
+    tweets_list = []
+    for search in search_terms:
+        for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{search} since:{since} until:{until}').get_items()): # declare username
+            tweets_list.append(tweet)
+    # Store all returned tweets in a dataframe
+    tweets_df = pd.DataFrame(tweets_list)    
+    # Alert notification & return dataframe
+    print("...tweets scraped.")
+    return(tweets_df)
+
 # Creating list and search parameters
-tweets_list = []
 search_terms = ("improvised explosive device", "IED", "improvised explosive", "explosive device", "suicide bomb",
                 "suicide bomber", "car bomb", "vbied", "pbied", "voied", "uas", "suas", "uav", "loitering munition",
                 "unmanned aerial system", "unmanned aerial vehicle", "drone attack", "drone explosive")
 since = "2021-09-01"
 until = "2021-12-01"
 
-# Use TwitterSearchScraper to append tweets to list
-for search in search_terms:
-    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{search} since:{since} until:{until}').get_items()): # declare username
-        tweets_list.append(tweet)
-        
-# Store all returned tweets in a dataframe
-tweets_df = pd.DataFrame(tweets_list)    
-
-# Alert notification
-print("...tweets scraped.")
+    
+# Call function
+tweets_df = search_twitter(search_terms, since, until)
 
 #%% Push data into dataframe, limit to month of November
 
@@ -82,7 +87,6 @@ tweets_df['date'] = pd.to_datetime(tweets_df['date'])
 # Filter for tweets occuring in November only, and export to CSV
 tweets = tweets_df.loc[(tweets_df['date'] >= '2021-11-01')
                                           & (tweets_df['date'] < '2021-12-01')]
-tweets.to_csv("C:\\Users\\natha\\Documents\\Master of Data Science\\MA5851 Natural Language Processing\\A3\\tweets.csv")
 
 #%% Cleaning
 
@@ -152,15 +156,105 @@ del tweets['place']
 del tweets['coordinates']
 del tweets['user_location']
 
+# Remove URLs from content and user description
+for i in (range(0, len(tweets))):
+    tweets['content'][i] = re.sub(r'http\S+', '', tweets['content'][i])
+for i in (range(0, len(tweets))):
+    tweets['user_desc'][i] = re.sub(r'http\S+', '', tweets['user_desc'][i])
+
+# Remove emojis in content, location user name and user description
+emoji_pattern = re.compile("["
+         u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                           "]+", flags=re.UNICODE)
+for i in (range(0, len(tweets))):
+    tweets['content'][i] = re.sub(emoji_pattern, '', tweets['content'][i])
+for i in (range(0, len(tweets))):
+    tweets['user_desc'][i] = re.sub(emoji_pattern, '', tweets['user_desc'][i])
+for i in (range(0, len(tweets))):
+    tweets['user_name'][i] = re.sub(emoji_pattern, '', tweets['user_name'][i])
+for i in (range(0, len(tweets))):
+    tweets['location'][i] = re.sub(emoji_pattern, '', tweets['location'][i])
+
 # Get dimensions
 print(tweets.shape) # 42008, 17
 
+# Split search terms into individual words
+terms = [word for term in search_terms for word in term.split()]
+terms = list(dict.fromkeys(terms))
+terms = [term.lower() for term in terms]
+
+# Find tweets correlating to search term
+key_words = []
+for tweet in tweets['content']:
+    review = []
+    words = tweet.split()
+    words = [word.lower() for word in words]
+    for word in words:
+        if word in terms:
+            review.append(word)
+    key_words.append(review)
+
+# Add new column onto tweets with class labels
+tweets['search_term'] = key_words
+
+# Get word frequency
+word_freq = []
+for cell in key_words:
+    for word in cell:
+        word_freq.append(word)
+word_freq = nltk.FreqDist(word_freq)
+
+# Get plot of words
+word_freq = pd.DataFrame({'Search Term':list(word_freq.keys()),
+                             'Count':list(word_freq.values())})
+g = word_freq.nlargest(columns="Count", n = 22) 
+plt.figure(figsize=(12,15)) 
+ax = sns.barplot(data=g, x= "Count", y = "Search Term") 
+ax.set(ylabel = 'Count') 
+plt.show()
+
+# Print examples
+print(tweets['content'][3])
+print(tweets['content'][88])
+
+# Remove rows with no search terms
+hold = []
+for word in key_words:
+    h = " ".join(map(str,word))
+    hold.append(h)
+tweets['temp'] = hold
+tweets = tweets.loc[(tweets['temp'] != "")]
+del tweets['temp']
+
+# Check dimensions following filtering
+print(tweets.shape) # 19923, 18
+
+# Reset index
+tweets = tweets.reset_index(drop=True)
+
 # Get number of unique users & mean posts
-print(tweets['user_name'].nunique()) #18109
+print(tweets['user_name'].nunique()) #15099
 all_users = nltk.FreqDist(tweets['user_name'])
 all_users = pd.DataFrame({'Username':list(all_users.keys()),
                              'Posts':list(all_users.values())})
-print(mean(all_users.values())) # approx 2.32
+print(mean(all_users['Posts'])) # approx 1.32
 
 # Get plot of top 30 users
 g = all_users.nlargest(columns="Posts", n = 30) 
@@ -188,21 +282,31 @@ ax.set(ylabel = 'count')
 plt.show()
 
 # Plot representation of location?
+locations = tweets['location']
+for i in (range(0, len(locations))):
+    locations[i] = re.sub(r'[^\w\s]', '', locations[i])
+word_list = []
+for i in range(0, len(locations)):
+    review = locations[i].split()
+    for word in review:
+        word_list.append(word)
+all_locations = nltk.FreqDist(word_list)
+all_locations = pd.DataFrame({'location':list(all_locations.keys()),
+                             'count':list(all_locations.values())})
+g = all_locations.nlargest(columns="count", n = 30) 
+plt.figure(figsize=(12,15)) 
+ax = sns.barplot(data=g, x= "count", y = "location") 
+ax.set(ylabel = 'count') 
+plt.show() # We can see pronouns, age, text unrelated to location
 
-# Clean/standardise location?
-
-# Plot number of tweets corresponding to each search term
-
-
+# Export final scrape to CSV
+tweets.to_csv("C:\\Users\\natha\\Documents\\Master of Data Science\\MA5851 Natural Language Processing\\A3\\tweets.csv")
+    
 #%% DOCUMENT THREE
 #
 #
 #
 #
 
-# Remove URLs from content and user description
-for i in (range(0, len(tweets))):
-    tweets['content'][i] = re.sub(r'http\S+', '', tweets['content'][i])
-for i in (range(0, len(tweets))):
-    tweets['user_desc'][i] = re.sub(r'http\S+', '', tweets['user_desc'][i])
+
 

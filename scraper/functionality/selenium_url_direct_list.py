dd4a895c2a78b8e0ea2e67367ac2864a17c3ef88
NathanKMuir <64637150+NathanKMuir@users.noreply.github.com>
2021-12-07 23:11:54 +1000

    Add files via upload

--- scraper/functionality/selenium_url_direct_list.py
+++ scraper/functionality/selenium_url_direct_list.py
@@ -8,27 +8,37 @@ Created on Tue Dec  7 20:03:07 2021
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
+import random
 
 # This script takes a given list of URLs and performs a Selenium scrape on it.
 urls = ["https://twitter.com/ASBMilitary/status/1466331358339469319",
         "https://twitter.com/DeGreenTribe/status/1468157962111881220",
         "https://twitter.com/pakhead/status/1468156397200101376"]
 
-# Define user agent
-
+# Define user agent list
+user_agent_list  = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',]
 
 # Set up scraper options
 options = webdriver.ChromeOptions()
 options.add_argument("--enable-javascript")
-driver = webdriver.Chrome(chrome_options=options)
 
 # Initialise hold list
-hold = []
+content_hold = []
+url_hold = []
 
 # Create loop
 for url in urls:
     
+    # Randomise  user agent
+    user_agent = random.choice(user_agent_list)
+    options.add_argument(f'user-agent={user_agent}')
+    
     # Execute browser
+    driver = webdriver.Chrome(chrome_options=options)
     driver.get(url)
     
     # Allow page to load
@@ -39,4 +49,6 @@ for url in urls:
     
     #To get the content of the tweet in the URL
     title = soup.get_text()
-    hold.append(title)
+    get_url = driver.current_url
+    content_hold.append(title)
+    url_hold.append(get_url)

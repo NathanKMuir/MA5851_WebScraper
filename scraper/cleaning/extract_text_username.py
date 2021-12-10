532e34d0c8ef6de6a345effd590e2b757256fd35
NathanKMuir <64637150+NathanKMuir@users.noreply.github.com>
2021-12-10 14:49:34 +1000

    Add files via upload

--- scraper/cleaning/extract_text_username.py
+++ scraper/cleaning/extract_text_username.py
@@ -74,6 +74,32 @@ scraped_tweets = scraped_tweets.loc[(scraped_tweets['char_count'] >= 0)
 del scraped_tweets['char_count']
 scraped_tweets.shape # 69468, 2
 
+# Delete last term in each string (screen name of next user)
+for i in range(0, len(scraped_tweets)-1):
+    try:
+        hold = scraped_tweets['text'][i]
+        hold = hold.split()
+        del hold[-1]
+        hold = " ".join(hold)
+        scraped_tweets['text'][i] = hold
+    except:
+        pass
+    
+# Remove new blank elements if tweet consisted of only one term
+scraped_tweets['text'] = scraped_tweets['text'].replace(r'^\s*$', np.NaN, regex=True)
+scraped_tweets.dropna(subset = ['text'], inplace = True) 
+scraped_tweets.shape # 65005, 2
+scraped_tweets = scraped_tweets.reset_index(drop=True)
+
+# Print example
+print(scraped_tweets['username'][388] + ": " + scraped_tweets['text'][388])
+# Rita_Katz: AQAP claims killing two Houthis in an IED attack on a motorcycle in Yemen's Abyan governorate,
+# marking the group's first claimed attack there since March 2021 (eight months ago).Quote TweetSITE Intel - Jihadist
+
+# Remove duplicates
+scraped_tweets = scraped_tweets.drop_duplicates(keep='first')
+scraped_tweets.shape # 19088, 2
+
 # Export as CSV
 scraped_tweets.to_csv("C:\\Users\\natha\\Documents\\Master of Data Science\\MA5851 Natural Language Processing\\A3\\scraped_tweets.csv")
     

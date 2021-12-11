ddc2545da816e6a8569383c13b74876f0ffa81cf
NathanKMuir <64637150+NathanKMuir@users.noreply.github.com>
2021-12-12 00:18:45 +1000

    Add files via upload
    Using the spacy package as the base, this script identifies named entities of type PERSON, ORGANISATION and GPE (location) and adds each row's values to the intel dataframe.

--- Models/Recommender System/named_entity_recognition.py
+++ Models/Recommender System/named_entity_recognition.py
@@ -21,30 +21,10 @@ del intel['Unnamed: 0']
 # Data preparation - remove # and @ from text
 intel['text'] = [re.sub(r'#', '', tweet) for tweet in intel['text']]
 
-#%% test
-
-test  = intel['text'][15]
-test = 'Irony as IED placed by Al Shabaab and Jeremy kills 15 of its own militants in Kenya  donaldtrump world american news'
-test1 = NER(test)
+# Get named entities for each observation (tweet)
 GPE_list = []
 PERSON_list = []
 ORG_list = []
-for word in test1.ents:
-    if word.label_ == "PERSON":
-        PERSON_list.append(word.text)
-    if word.label_ == "GPE":
-        GPE_list.append(word.text)
-    if word.label_ == "ORG":
-        ORG_list.append(word.text)
-    else:
-        pass
-    
-#%% test bigger
-
-GPE_list = []
-PERSON_list = []
-ORG_list = []
-
 for i in range(0, len(intel)):
     GPE_temp = []
     PERSON_temp = []
@@ -60,3 +40,11 @@ for i in range(0, len(intel)):
     GPE_list.append(GPE_temp)
     PERSON_list.append(PERSON_temp)
     ORG_list.append(ORG_temp)
+    
+# Add named entities onto intel dataframe
+intel['location'] = GPE_list
+intel['person'] = PERSON_list
+intel['organisation'] = ORG_list
+
+# Export intel to csv
+intel.to_csv("C:\\Users\\natha\\Documents\\Master of Data Science\\MA5851 Natural Language Processing\\A3\\intel_NER.csv")
